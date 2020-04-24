c1730221c75c65f886862afe98c25262203532a7
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-04-24 14:30:46 +0200

    Setup basic logging and restructure main program

---
+++ lib/logs.py
@@ -0,0 +1,30 @@
+from logging import (
+    DEBUG, ERROR, INFO, WARNING, Formatter, StreamHandler, getLogger
+)
+
+FORMATTER = Formatter('''
+| %(levelname)s - %(asctime)s | %(name)s | %(processName)s %(threadName)s
+| %(module)s.%(funcName)s [%(pathname)s:%(lineno)d]
+%(message)s
+'''.lstrip())
+
+LOG_LEVELS = {
+    'debug': DEBUG,
+    'error': ERROR,
+    'info': INFO,
+    'warn': WARNING,
+    'warning': WARNING,
+}
+
+
+def setup_logging(level_name):
+    root_logger = getLogger()
+    root_logger.setLevel(DEBUG)
+
+    level = LOG_LEVELS.get(level_name, DEBUG)
+
+    stream = StreamHandler(stream=None)
+    stream.setFormatter(FORMATTER)
+    stream.setLevel(level)
+
+    root_logger.addHandler(stream)
--- printed.py
+++ printed.py
@@ -1,16 +1,22 @@
-import sys
-
 import click
 
+from lib.logs import LOG_LEVELS, setup_logging
 CLI_PROG = 'printed'
 CLI_VERS = '0.0.0'
 
 
 @click.command()
 @click.version_option(prog_name=CLI_PROG, version=CLI_VERS)
-def main():
-    return 0
+@click.option(
+    '-v', '--level', 'level', envvar='LEVEL',
+    type=click.Choice(LOG_LEVELS.keys(), case_sensitive=True),
+    default='warning', show_default=True,
+    help='Control logging level',
+)
+def main(**cargs):
+    setup_logging(cargs['level'])
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    # pylint: disable=no-value-for-parameter
+    main()
